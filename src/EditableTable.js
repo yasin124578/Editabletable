7ab30fcd97bb43e4d8f0cd8bb4e2c63747042d77
Yasin Ghadiri <yasin124578124578@gmail.com>
2020-02-27 10:55:19 +0330

    WIP

---
+++ src/EditableTable.js
@@ -0,0 +1,213 @@
+import {Table, Input, InputNumber, Popconfirm, Form, Card, button, Button} from 'antd';
+import React from 'react';
+
+const data = [];
+for (let i = 0; i < 100; i++) {
+    data.push({
+        radif: i.toString(),
+        type: "phone",
+        account: `Edrward ${i}`,
+        situation: '2',
+        use: `London Park no. ${i}`,
+    });
+}
+const EditableContext = React.createContext();
+
+class EditableCell extends React.Component {
+    getInput = () => {
+        if (this.props.inputType === 'number') {
+            return <InputNumber />;
+        }
+        return <Input />;
+    };
+
+    renderCell = ({ getFieldDecorator }) => {
+        const {
+            editing,
+            dataIndex,
+            title,
+            inputType,
+            record,
+            index,
+            children,
+            ...restProps
+        } = this.props;
+        return (
+            <td {...restProps}>
+                {editing ? (
+                    <Form.Item style={{ margin: 0 }}>
+                        {getFieldDecorator(dataIndex, {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: `Please Input ${title}!`,
+                                },
+                            ],
+                            initialValue: record[dataIndex],
+                        })(this.getInput())}
+                    </Form.Item>
+                ) : (
+                    children
+                )}
+            </td>
+        );
+    };
+
+    render() {
+        return <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>;
+    }
+}
+
+class EditableTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data, editingKey: '' };
+        this.columns = [
+            {
+                title: 'radif',
+                dataIndex: 'radif',
+                width: '6%',
+                editable: true,
+            },
+            {
+                title: 'type',
+                dataIndex: 'type',
+                width: '12%',
+                editable: true,
+            },
+            {
+                title: 'account',
+                dataIndex: 'account',
+                width: '25%',
+                editable: true,
+            },
+            {
+                title: 'situation',
+                dataIndex: 'situation',
+                width: '9%',
+                editable: true,
+            },
+            {
+                title: 'checker',
+                dataIndex: 'checker',
+                width: '18%',
+                editable: true,
+            },
+            {
+                title: 'use',
+                dataIndex: 'use',
+                width: '18%',
+                editable: true,
+            },
+            {
+                title: 'operation',
+                dataIndex: 'operation',
+                width:"7%",
+                render: (text, record) => {
+                    const { editingKey } = this.state;
+                    const editable = this.isEditing(record);
+                    return editable ? (
+                        <span>
+              <EditableContext.Consumer>
+                {form => (
+                    <a
+                        onClick={() => this.save(form, record.key)}
+                        style={{ marginRight: 8 }}
+                    >
+                        Save
+                    </a>
+                )}
+              </EditableContext.Consumer>
+              <Popconfirm title="Sure to cancel?" onConfirm={() => this.cancel(record.key)}>
+                <a>Cancel</a>
+              </Popconfirm>
+            </span>
+                    ) : (
+                        <a disabled={editingKey !== ''} onClick={() => this.edit(record.key)}>
+                            Edit
+                        </a>
+                    );
+                },
+            },
+        ];
+    }
+
+    isEditing = record => record.key === this.state.editingKey;
+
+    cancel = () => {
+        this.setState({ editingKey: '' });
+    };
+
+    save(form, key) {
+        form.validateFields((error, row) => {
+            if (error) {
+                return;
+            }
+            const newData = [...this.state.data];
+            const index = newData.findIndex(item => key === item.key);
+            if (index > -1) {
+                const item = newData[index];
+                newData.splice(index, 1, {
+                    ...item,
+                    ...row,
+                });
+                this.setState({ data: newData, editingKey: '' });
+            } else {
+                newData.push(row);
+                this.setState({ data: newData, editingKey: '' });
+            }
+        });
+    }
+
+    edit(key) {
+        this.setState({ editingKey: key });
+    }
+
+    render() {
+        const components = {
+            body: {
+                cell: EditableCell,
+            },
+        };
+
+        const columns = this.columns.map(col => {
+            if (!col.editable) {
+                return col;
+            }
+            return {
+                ...col,
+                onCell: record => ({
+                    record,
+                    inputType: col.dataIndex === 'age' ? 'number' : 'text',
+                    dataIndex: col.dataIndex,
+                    title: col.title,
+                    editing: this.isEditing(record),
+                }),
+            };
+        });
+
+        return (
+            <div>
+            <Card style={{width:300}}>
+                <Button type='primary' shape='circle'>add</Button>
+            </Card>
+            <EditableContext.Provider value={this.props.form}>
+                <Table
+                    components={components}
+                    bordered
+                    dataSource={this.state.data}
+                    columns={columns}
+                    rowClassName="editable-row"
+                    pagination={{
+                        onChange: this.cancel,
+                    }}
+                />
+            </EditableContext.Provider>
+            </div>
+        );
+    }
+}
+
+const EditableFormTable = Form.create()(EditableTable);
+
+export default EditableFormTable ;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'antd/dist/antd.css';
+import EditableFormTable  from "./EditableTable";
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<EditableFormTable/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
